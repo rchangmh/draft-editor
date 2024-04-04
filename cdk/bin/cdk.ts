d1ad97bac0eb9876125294dd4567dde8ec6d57b6
Ronald Chang <ronald.chang@pulseanalyticsapp.com>
2024-04-03 21:30:32 -0400

    updated cdk

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,14 +5,15 @@ import { StaticStack } from '../lib/static-stack'
 import * as path from 'path'
 
 const app = new cdk.App()
-const draftStack = new StaticStack(app, 'draftStack', {
+
+new StaticStack(app, 'draftStack', {
   staticAssetsPath: path.join(__dirname, '..', '..', 'svelte', 'public'),
   hostedZoneId: 'Z03304211EX845FJI16N9',
   subdomainName: 'draft',
   zoneName: 'cdk.link',
 })
 
-const betaDraftStack = new StaticStack(app, 'betaDraftStack', {
+new StaticStack(app, 'betaDraftStack', {
   staticAssetsPath: path.join(__dirname, '..', '..', 'svelte', 'public'),
   hostedZoneId: 'Z03304211EX845FJI16N9',
   subdomainName: 'beta.draft',
